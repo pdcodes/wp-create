abedcabc56348ce8067397050a5143d6f4faa0ee
Peter <peter@bluetext.com>
2017-10-10 21:13:06 -0400

    committing to repo

---
+++ autocreate.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#pwd
+
+#echo "$1 just wrote a shell script."
+
+#echo "What is your name?"
+#read name
+#echo "$name"
+
+echo "What is the name of the site you'd like to create?"
+read sitename
+
+terminus site:create --org Bluetext "$sitename" "$sitename" e8fe8550-1ab9-4964-8838-2b9abdccf4bf
