65edce7ca9768c66ed317d075135717b2939017d
Peter <peter@bluetext.com>
2017-10-10 21:19:11 -0400

    updating script

--- autocreate.sh
+++ autocreate.sh
@@ -12,3 +12,9 @@ echo "What is the name of the site you'd like to create?"
 read sitename
 
 terminus site:create --org Bluetext "$sitename" "$sitename" e8fe8550-1ab9-4964-8838-2b9abdccf4bf
+
+echo "CMS deploy completed;"
+
+terminus connection:set "$sitename".dev git
+
+echo "Connection mode set to git;"
